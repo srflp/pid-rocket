3bdbd849f0015e985909b976ac2cb2a8f647d4c3
Filip Sauer <sauer.filip@gmail.com>
2021-01-11 12:57:39 +0100

    Tweak default parameters

--- src/computations/pid/typesAndDefaults.ts
+++ src/computations/pid/typesAndDefaults.ts
@@ -22,7 +22,18 @@ export const defaultOptions = {
   simTime: 0.1,
   maxThrust: 15,
   destination: 110,
-  kP: 0.36,
-  kI: 40.0,
-  kD: 0.0008099999999999997,
+  kP: 0.335,
+  kI: 18.853,
+  kD: 0.00079,
 };
+
+// original ones
+// export const defaultOptions = {
+//   timeStep: 0.001,
+//   simTime: 0.1,
+//   maxThrust: 15,
+//   destination: 110,
+//   kP: 0.36,
+//   kI: 40.0,
+//   kD: 0.0008099999999999997,
+// };
