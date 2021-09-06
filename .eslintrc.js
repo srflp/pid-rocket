84e253677a41ab46b7de87702cb6c78278b2a117
Filip Sauer <sauer.filip@gmail.com>
2021-09-06 03:39:39 +0200

    chore: use next lint and next's eslint configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,9 @@
 module.exports = {
   extends: [
-    'prettier',
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
+    'next/core-web-vitals',
+    'prettier',
   ],
-  parserOptions: {
-    project: './tsconfig.json',
-    ecmaVersion: 7,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint'],
 };
